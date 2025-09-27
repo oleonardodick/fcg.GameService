436d2030f2558a5e4a272e590f91e40a92aa94f9
Leonardo Bernardes <leonardod.bernardes@gmail.com>
2025-09-27 15:41:03 -0300

    Adicionado logs e grafana cloud

--- src/fcg.GameService.API/Controllers/GameLibraryController.cs
+++ src/fcg.GameService.API/Controllers/GameLibraryController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using fcg.GameService.Application.Helpers;
 using fcg.GameService.Application.Interfaces;
 using fcg.GameService.Domain.Exceptions;
@@ -18,17 +19,20 @@ namespace fcg.GameService.API.Controllers
         private readonly IValidator<CreateGameLibraryDTO> _validatorCreate;
         private readonly IValidator<AddGameToLibraryDTO> _validatorAddGame;
         private readonly IValidator<RemoveGameFromLibraryDTO> _validatorRemoveGame;
+        private readonly IAppLogger<GameLibraryController> _logger;
 
         public GameLibraryController(
             IGameLibraryUseCase gameLibraryUseCase,
             IValidator<CreateGameLibraryDTO> validatorCreate,
             IValidator<AddGameToLibraryDTO> validatorAddGame,
-            IValidator<RemoveGameFromLibraryDTO> validatorRemoveGame)
+            IValidator<RemoveGameFromLibraryDTO> validatorRemoveGame,
+            IAppLogger<GameLibraryController> logger)
         {
             _gameLibraryUseCase = gameLibraryUseCase;
             _validatorCreate = validatorCreate;
             _validatorAddGame = validatorAddGame;
             _validatorRemoveGame = validatorRemoveGame;
+            _logger = logger;
         }
 
         [HttpGet("{id}")]
@@ -48,9 +52,18 @@ namespace fcg.GameService.API.Controllers
                 );
             }
 
-            var gameLibrary = await _gameLibraryUseCase.GetByIdAsync(id);
-
-            return Ok(gameLibrary);
+            _logger.LogInformation("Iniciando a busca da biblioteca com o ID {id}", id);
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                var gameLibrary = await _gameLibraryUseCase.GetByIdAsync(id);
+                return Ok(gameLibrary);
+            }
+            finally
+            {   
+                stopwatch.Stop();
+                _logger.LogInformation("Finalizou a busca da biblioteca com o ID {id} após {duration}ms", id, stopwatch.ElapsedMilliseconds);
+            }
         }
 
         [HttpGet("user/{userId}")]
@@ -69,27 +82,46 @@ namespace fcg.GameService.API.Controllers
                     error
                 );
             }
-                
-            var gameLibrary = await _gameLibraryUseCase.GetByUserIdAsync(userId);
 
-            return Ok(gameLibrary);
+            _logger.LogInformation("Iniciando a busca da biblioteca do usuário com o ID {id}", userId);
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                var gameLibrary = await _gameLibraryUseCase.GetByUserIdAsync(userId);
+                return Ok(gameLibrary);
+            }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogInformation("Finalizou a busca da biblioteca do usuário com o ID {id} após {duration}ms",
+                    userId,
+                    stopwatch.ElapsedMilliseconds
+                );
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateGameLibraryDTO gameLibrary)
         {
-            var errors = ValidationHelper.Validate(_validatorCreate, gameLibrary);
-
-            if (errors.Count > 0)
-                throw new AppValidationException(errors);
-                
-            var createdGameLibrary = await _gameLibraryUseCase.CreateAsync(gameLibrary);
-
-            return CreatedAtAction(
-                nameof(GetById),
-                new { id = createdGameLibrary.Id },
-                createdGameLibrary
-            );
+            _logger.LogInformation("Iniciando a criação da biblioteca do usuário {id}", gameLibrary.UserId);
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                var errors = ValidationHelper.Validate(_validatorCreate, gameLibrary);
+                if (errors.Count > 0)
+                    throw new AppValidationException(errors);
+                var createdGameLibrary = await _gameLibraryUseCase.CreateAsync(gameLibrary);
+                return CreatedAtAction(
+                    nameof(GetById),
+                    new { id = createdGameLibrary.Id },
+                    createdGameLibrary
+                );
+            }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogInformation("Criação da biblioteca do usuário {id} finalizada após {duration}ms", gameLibrary.UserId, stopwatch.ElapsedMilliseconds);
+            }
         }
 
         [HttpPost("{libraryId}/addGame")]
@@ -108,29 +140,48 @@ namespace fcg.GameService.API.Controllers
                     error
                 );
             }
-
-            if (await _gameLibraryUseCase.ExistsGameOnLibraryAsync(libraryId, game.Id))
+            
+            _logger.LogInformation("Iniciando a busca se o jogo já existe na biblioteca de Id {id}.", libraryId);
+            var stopwatch = Stopwatch.StartNew();
+            try
             {
-                List<ErrorDetails> error = [
-                    new ErrorDetails {
-                        Property = nameof(game.Id),
-                        Errors = ["Jogo já cadastrado."]
-                    }
-                ];
-                
-                throw new AppValidationException(
-                    error
-                );
+                if (await _gameLibraryUseCase.ExistsGameOnLibraryAsync(libraryId, game.Id))
+                {
+                    List<ErrorDetails> error = [
+                        new ErrorDetails {
+                            Property = nameof(game.Id),
+                            Errors = ["Jogo já cadastrado."]
+                        }
+                    ];
+
+                    throw new AppValidationException(
+                        error
+                    );
+                }
             }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogInformation("Finalizou a busca se o jogo já existe na biblioteca de ID {id} após {duration}ms", libraryId, stopwatch.ElapsedMilliseconds);
+            }
+            
 
-            var errors = ValidationHelper.Validate(_validatorAddGame, game);
-
-            if (errors.Count > 0)
-                throw new AppValidationException(errors);
-
-            await _gameLibraryUseCase.AddGameToLibraryAsync(libraryId, game);
-
-            return NoContent();
+            _logger.LogInformation("Iniciando a inclusão de jogo na biblioteca de Id {id}.", libraryId);
+            stopwatch.Reset();
+            stopwatch.Start();
+            try
+            {
+                var errors = ValidationHelper.Validate(_validatorAddGame, game);
+                if (errors.Count > 0)
+                    throw new AppValidationException(errors);
+                await _gameLibraryUseCase.AddGameToLibraryAsync(libraryId, game);
+                return NoContent();
+            }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogInformation("Finalizou a inclusão de jogo na biblioteca de Id {id} após {duration}ms.", libraryId, stopwatch.ElapsedMilliseconds);
+            }
         }
 
         [HttpDelete("{libraryId}/removeGame")]
@@ -154,11 +205,28 @@ namespace fcg.GameService.API.Controllers
 
             if (errors.Count > 0)
                 throw new AppValidationException(errors);
-
+                
+            _logger.LogInformation("Iniciando a busca se o jogo de ID {id} existe na biblioteca de id {libraryId}.", game.Id, libraryId);
+            var stopwatch = Stopwatch.StartNew();
             if (!await _gameLibraryUseCase.ExistsGameOnLibraryAsync(libraryId, game.Id))
-                    return NotFound("Jogo não cadastrado");
+                return NotFound("Jogo não cadastrado");
+            stopwatch.Stop();
+            _logger.LogInformation("Finalizou a busca se o jogo de ID {id} existe na biblioteca de id {libraryId} após {duration}ms.",
+                game.Id,
+                libraryId,
+                stopwatch.ElapsedMilliseconds
+            );
 
-            var success = await _gameLibraryUseCase.RemoveGameFromLibraryAsync(libraryId, game);
+            _logger.LogInformation("Iniciando a exclusão do jogo de ID {id} na biblioteca de id {libraryId}.", game.Id, libraryId);
+            stopwatch.Reset();
+            stopwatch.Start();
+            await _gameLibraryUseCase.RemoveGameFromLibraryAsync(libraryId, game);
+            stopwatch.Stop();
+            _logger.LogInformation("Finalizou a exclusão do jogo de ID {id} na biblioteca de id {libraryId} após {duration}ms.",
+                game.Id,
+                libraryId,
+                stopwatch.ElapsedMilliseconds
+            );
 
             return NoContent();
         }
--- src/fcg.GameService.API/Controllers/Purchase.cs
+++ src/fcg.GameService.API/Controllers/Purchase.cs
@@ -1,4 +1,5 @@
-﻿using fcg.GameService.Application.Helpers;
+﻿using System.Diagnostics;
+using fcg.GameService.Application.Helpers;
 using fcg.GameService.Application.Interfaces;
 using fcg.GameService.Domain.Exceptions;
 using fcg.GameService.Domain.Models;
@@ -14,19 +15,33 @@ namespace fcg.GameService.API.Controllers;
 [Consumes("application/json")]
 public class Purchase(
     IPurchaseUseCase purchaseUseCase,
-    IValidator<PurchaseGameDTO> validatorPurchase) : ControllerBase
+    IValidator<PurchaseGameDTO> validatorPurchase,
+    IAppLogger<Purchase> logger) : ControllerBase
 {
     private readonly IPurchaseUseCase _purchaseUseCase = purchaseUseCase;
     private readonly IValidator<PurchaseGameDTO> _validatorPurchase = validatorPurchase;
+    private readonly IAppLogger<Purchase> _logger = logger;
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] PurchaseGameDTO purchase, CancellationToken cancellationToken)
     {
-        List<ErrorDetails> errors = ValidationHelper.Validate(_validatorPurchase, purchase);
-
-        if (errors.Count > 0)
-            throw new AppValidationException(errors);
-
-        return Ok(await _purchaseUseCase.PublishAsync(purchase, cancellationToken));
+        _logger.LogInformation("Iniciando a compra do jogo com {id} pelo usuário {userId}", purchase.GameId, purchase.UserId);
+        var stopwatch = Stopwatch.StartNew();
+        try
+        {
+            List<ErrorDetails> errors = ValidationHelper.Validate(_validatorPurchase, purchase);
+            if (errors.Count > 0)
+                throw new AppValidationException(errors);
+            return Ok(await _purchaseUseCase.PublishAsync(purchase, cancellationToken));
+        }
+        finally
+        {
+            stopwatch.Stop();
+            _logger.LogInformation("Finalizando a compra do jogo com {id} pelo usuário {userId} após {duration}ms",
+                purchase.GameId,
+                purchase.UserId,
+                stopwatch.ElapsedMilliseconds
+            );
+        }
     }
 }
